8bbdc161f420129c02dc14d1424f8b03df6e5e15
shi.changliang <shi.changliang@zhaopin.com.cn>
2022-05-31 17:50:16 +0800

    refactor:调整代码结构

--- core/cmd/server.go
+++ core/cmd/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/ichaly/go-easy/core/graph"
@@ -10,17 +11,28 @@ import (
 	"github.com/ichaly/go-env"
 )
 
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/api")
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+func graphqlHandler() gin.HandlerFunc {
+	h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
 func main() {
 	port, _ := env.String(":${PORT:=8080}")
 	r := gin.New()
 	r.Use(cors.Default())
-	r.POST("/api", func() gin.HandlerFunc {
-		h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
-		return func(c *gin.Context) {
-			h.ServeHTTP(c.Writer, c.Request)
-		}
-	}())
+	r.GET("/", playgroundHandler())
+	r.POST("/api", graphqlHandler())
 	fmt.Println("Now server is running on port 8080")
-	fmt.Println("Test with Get      : curl -g 'http://localhost:8080/api?query={hello}'")
+	fmt.Printf("Connect to http://localhost:%s/ for GraphQL playground\n", port)
+	fmt.Println("Test with Get: curl -g 'http://localhost:8080/api?query={hello}'")
 	_ = r.Run(port)
 }
